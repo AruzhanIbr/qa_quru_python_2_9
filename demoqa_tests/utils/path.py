a67375f29bfc5f1298be0da10edb7622b1416072
AruzhanIbr <ibragimova080499@gmail.com>
2022-10-02 22:04:22 +0600

    commit

--- demoqa_tests/utils/path.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from selene.support.shared import browser
-import resources
-
-
-def upload_picture(relative_path):
-    path = str(Path(resources.__file__).parent.joinpath(relative_path).absolute())
-    browser.element('#uploadPicture').send_keys(path)
